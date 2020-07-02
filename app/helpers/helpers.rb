30256eeb6ab4698833ac316f9114826314052c1f
dwandrew <danwandrew@gmail.com>
2020-07-02 09:42:36 +0100

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+
+    def self.current_user(session)
+        # id= session[:user_id]
+        User.find(session[:user_id])
+    end
+
+    def self.is_logged_in?(session)
+        !!session[:user_id]
+    end
 end
