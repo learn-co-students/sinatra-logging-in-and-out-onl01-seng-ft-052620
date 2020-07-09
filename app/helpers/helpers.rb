87d6c56aca388b1ee54d95a95c2e003479bfa1f1
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-09 20:12:08 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,17 @@
+
 class Helpers
+
+  def self.current_user(session)
+    @current = User.find(session[:user_id])
+    @current
+  end
+
+  def self.is_logged_in?(session)
+    if session[:user_id] == self.user_id
+      TRUE
+    else
+      FALSE
+    end
+  end
+
 end
