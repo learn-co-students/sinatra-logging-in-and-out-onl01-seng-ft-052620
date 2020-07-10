a89da155a19ee6a4d6e4fb603de5d10a96098c0c
francepants <franceline90@gmail.com>
2020-07-09 20:44:26 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+    def self.current_user(session)
+        User.find(session[:user_id])
+    end
+
+    def self.is_logged_in?(session)
+        # session[:user_id] ? true : false
+        #In most programming languages, including Ruby, ! will return the opposite of the boolean value of the operand. So when you chain two exclamation marks together, it converts the value to a boolean.
+        !!session[:user_id]
+    end
 end
