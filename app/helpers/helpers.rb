0c4722bc8f9a38bd9ca1b4d04e17507859bff481
JohnAjaka <johnajaka96@gmail.com>
2020-07-15 00:32:08 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+    def self.current_user(sesh_hash)
+        @user = User.find(sesh_hash[:user_id])
+    end
+
+    def self.is_logged_in?(sesh_hash)
+        !!sesh_hash[:user_id]
+    end
 end
